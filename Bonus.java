82e34c6617d097a569d636e302f295375c94f0bd
Nhu Nguyen <nhu313@gmail.com>
2018-05-24 11:42:45 -0400

    Added readme and comments on assignment

--- Bonus.java
+++ Bonus.java
@@ -1,22 +1,40 @@
-
-/**
- * Write a description of class Bonus here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Bonus
 {
-    public String acronym(String phrase) {
-        return null;
-    }
-    
-    public String encrypt(String word) {
-        return null;
-    }
-   
-    
-    public int factorial(int number){
-        return -1;
-    }
+
+      /**
+       * Given a number, return the factorial of that number.
+       * For example, given 5, the factorial is 5 x 4 x 3 x 2 x 1 which should return 120.
+       * @param number
+       * @return the factorial of the number
+       */
+      public int factorial(int number){
+          return -1;
+      }
+
+      /**
+       * Given a phrase, get the acronym of that phrase. Acronym is the combination of
+       * the first character of each word in upper case.
+       * For example, given "Ruby on Rails", this method will return "ROR"
+       * @param phrase
+       * @return Upper case of the first letter of each word
+       */
+      public String acronym(String phrase) {
+          return null;
+      }
+
+      /**
+       * To prevent anyone from reading our messages, we can encrypt it so it will not be readable by anyone except it
+       * intended audience. This method encrypt the message by shifting the character by 3 characters. If the character is
+       * at the end of the alphabet, it will wraps around.
+       * For example:
+       *  'a' => 'd'
+       *  'w' => 'z'
+       *  'x' => 'a'
+       *  'y' => 'b'
+       * @param word
+       * @return the encrypted string by shifting each character by three character
+       */
+      public String encrypt(String word) {
+          return null;
+      }
 }
